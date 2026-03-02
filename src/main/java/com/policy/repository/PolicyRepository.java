624f353a82be324875bd363fda7e51ef8d7e4401
learn-grow-repeat <swathymenon.316@gmail.com>
2026-03-02 16:38:45 +0000

    Create PolicyRepository.java

---
+++ src/main/java/com/policy/repository/PolicyRepository.java
@@ -0,0 +1,10 @@
+package com.example.policy.repository;
+
+import com.example.policy.dao.Policy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface PolicyRepository extends JpaRepository<Policy,String> {
+ boolean existsByPolicyNumber(String policyNumber);
+ Optional<Policy> findByPolicyNumber(String policyNumber);
+}
