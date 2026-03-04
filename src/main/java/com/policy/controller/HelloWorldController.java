4f6aa32898fef37922b588df1d86ef4636b72274
learn-grow-repeat <swathymenon.316@gmail.com>
2026-03-04 18:59:09 +0000

    Update HelloWorldController.java

--- src/main/java/com/policy/controller/HelloWorldController.java
+++ src/main/java/com/policy/controller/HelloWorldController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController { 
    @GetMapping("/hello")
   public String index() {
-      System.out.println("HelloWorldController-hello!!!")
+      System.out.println("HelloWorldController-hello!!!");
     return "Greetings from Spring Boot!";
   }
   @GetMapping("/")
   public String index1() {
-     System.out.println("Greetings Swathy Sudha!")
+     System.out.println("Greetings Swathy Sudha!");
     return "Greetings Swathy Sudha!";
   }
 
