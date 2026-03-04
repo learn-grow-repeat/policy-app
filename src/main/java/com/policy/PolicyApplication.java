a0f0779bb1821575c15e2794cd5d76619968039b
learn-grow-repeat <swathymenon.316@gmail.com>
2026-03-04 18:51:35 +0000

    Update PolicyApplication.java

--- src/main/java/com/policy/PolicyApplication.java
+++ src/main/java/com/policy/PolicyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PolicyApplication {
   public static void main(String[] args) {
+    System.out.println("Main java application!!!")
     SpringApplication.run(PolicyApplication.class, args);
   }
 }
